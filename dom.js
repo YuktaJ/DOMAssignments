5ecb97dd38a5dcdb2058eb1c942832e273fa93eb
YuktaJ <yuktapatil1820@gmail.com>
2023-08-18 12:46:33 +0530

    Assignment 7.

--- dom.js
+++ dom.js
@@ -44,8 +44,8 @@
 // }
 
 // QUERY SELECTOR //
-var header = document.querySelector('#main-header');
-header.style.borderBottom = "solid 3px #ccc";
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = "solid 3px #ccc";
 
 //Trying to edit item 5 using getElementsByClassName//
 // var item_5 = document.getElementsByClassName('list-group-item');
@@ -58,17 +58,89 @@ header.style.borderBottom = "solid 3px #ccc";
 // fifth_item.textContent = "Hello Item 5";
 
 // Changing second item color to green
-var second_item = document.querySelector(".list-group-item:nth-child(2)");
-second_item.style.backgroundColor = 'green';
-second_item.style.color = 'green';
+// var second_item = document.querySelector(".list-group-item:nth-child(2)");
+// second_item.style.backgroundColor = 'green';
+// second_item.style.color = 'green';
 
-//making the third item invisible 
-var third_item = document.querySelector(".list-group-item:nth-child(3)");
-third_item.style.display = 'none';
+// //making the third item invisible 
+// var third_item = document.querySelector(".list-group-item:nth-child(3)");
+// third_item.style.display = 'none';
 
-//Making all odd number item's bg color green 
-var odd = document.querySelectorAll('li:nth-child(odd)');
-for (var i = 0; i < odd.length; i++) {
-    odd[i].style.backgroundColor = 'green';
-    //changes odd number items bg color 
-}
+// //Making all odd number item's bg color green 
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// for (var i = 0; i < odd.length; i++) {
+//     odd[i].style.backgroundColor = 'green';
+//     //changes odd number items bg color 
+// }
+
+// TRAVERSING THE DOM
+ var itemList = document.querySelector('#items');
+// parentNode property 
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4'
+
+//parentElement property 
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4'
+
+//childNodes property 
+//console.log(itemList.childNodes);
+
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+
+// itemList.children[1].style.backgroundColor = "yellow";
+
+//firstChild property 
+//console.log(itemList.firstChild)
+
+//firstElementChild property important 
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = "Hello 1"
+
+//lastChild property 
+// console.log(itemList.lastChild);
+
+//lastElementChild property important
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello 4';
+
+// nextSibling
+// console.log(itemList.nextSibling); gives text as output in console
+
+//nextElementSibling
+//  console.log(itemList.nextElementSibling); 
+// gives output null because ul has no further siblings 
+
+//previousSibling 
+//console.log(itemList.previousSibling);
+
+// previousElementSibling 
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = "green"
+
+// createElement
+var newDiv = document.createElement('div');
+
+//Add a class
+newDiv.className='Hello';
+
+//Add id
+newDiv.id  = "Hello1";
+
+//Add attribute 
+newDiv.setAttribute('title','Hello Div');
+
+//create a text node 
+var newDivText = document.createTextNode("Hello World");
+
+//Adding the above text to newly created div 
+newDiv.appendChild(newDivText);
+
+//Adding the above created element to the dom 
+var container = document.querySelector("header .container");
+var h1 = document.querySelector('header h1');
+console.log(newDiv);
+container.insertBefore(newDiv, h1);
+
+//
